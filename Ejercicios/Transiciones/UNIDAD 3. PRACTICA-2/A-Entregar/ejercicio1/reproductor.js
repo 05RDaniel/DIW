050e53957d31e93b9ebc68d70c40a879e9da5136
Daniel <daniel.rufes@gmail.com>
2025-01-14 21:02:24 +0100

    Cozaz

--- Ejercicios/Transiciones/UNIDAD 3. PRACTICA-2/A-Entregar/ejercicio1/reproductor.js
+++ Ejercicios/Transiciones/UNIDAD 3. PRACTICA-2/A-Entregar/ejercicio1/reproductor.js
@@ -35,7 +35,7 @@ function accionPlay()
 	else
 	{
 		medio.play();
-		play.value='||';
+		play.value='▮▮';
 		bucle=setInterval(redimensionaBarra, 1000);
 	}
 }
@@ -48,12 +48,61 @@ function iniciar()
 	barra=document.getElementById('barra');
 	progreso=document.getElementById('progreso');
 	play=document.getElementById('play');
+	silence=document.getElementById('silenciar');
+	volumen=document.getElementById('volumen');
     /* obtener los objetos del resto de elementos necesarios */
 	
 	play.addEventListener('click', accionPlay, false);
 	/* crear los manejadores de eventos para el resto de botones */
 
 	barra.addEventListener('click', desplazarMedio, false);
+}
+
+function reiniciar(){
+	medio.currentTime=0;
+    progreso.style.width='0px';
+    play.value='\u25BA';
+    window.clearInterval(bucle);
+    medio.pause();
+    medio.load();
+    redimensionaBarra();
+	accionPlay();
+}
+
+function retroceder(){
+	var nuevoTiempo=medio.currentTime-5;
+    if(nuevoTiempo<0) nuevoTiempo=0;
+    medio.currentTime=nuevoTiempo;
+    progreso.style.width=nuevoTiempo*maximo / medio.duration+'px';
+}
+
+function avanzar(){
+	var nuevoTiempo=medio.currentTime+5;
+    if(nuevoTiempo<0) nuevoTiempo=0;
+    medio.currentTime=nuevoTiempo;
+    progreso.style.width=nuevoTiempo*maximo / medio.duration+'px';
+}
+
+function silenciar(){
+	if(silence.value == "silenciar"){
+	medio.volume = 0;
+	silence.value = "desilenciar"
+	} else {
+		medio.volume = 1;
+		silence.value = "silenciar";
+	}
+}
+
+function volumeUp(){
+	if(medio.volume < 1){
+    medio.volume += 0.1;
+    }
+}
+
+function volumeDown(){
+	if(medio.volume > 0){
+    medio.volume -= 0.1;
+    }
 }
 
 window.addEventListener('load', iniciar, false);
