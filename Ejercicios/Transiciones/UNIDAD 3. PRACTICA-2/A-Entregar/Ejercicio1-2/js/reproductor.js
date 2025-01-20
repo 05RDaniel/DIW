45c6e7a65d3e8953c067ad13c039e9effcad00cc
Daniel <daniel.rufes@gmail.com>
2025-01-20 16:18:40 +0100

    Cozaz

---
+++ Ejercicios/Transiciones/UNIDAD 3. PRACTICA-2/A-Entregar/Ejercicio1-2/js/reproductor.js
@@ -0,0 +1,118 @@
+function redimensionaBarra()
+{
+	if(!medio.ended)
+	{
+		var total=parseInt(medio.currentTime*maximo / medio.duration);
+		progreso.style.width=total+'px';
+	}
+	else
+	{
+		progreso.style.width='0px';
+		play.value='\u25BA';
+		window.clearInterval(bucle);
+	}
+}
+
+function desplazarMedio(e)
+{
+	if(!medio.paused && !medio.ended)
+	{
+		var ratonX=e.pageX-barra.offsetLeft;
+		var nuevoTiempo=ratonX*medio.duration/maximo;
+		medio.currentTime=nuevoTiempo;
+		progreso.style.width=ratonX+'px';
+	}
+}
+
+function accionPlay()
+{
+	if(!medio.paused && !medio.ended) 
+	{
+		medio.pause();
+		play.value='\u25BA';
+		window.clearInterval(bucle);
+	}
+	else
+	{
+		medio.play();
+		play.value='▮▮';
+		bucle=setInterval(redimensionaBarra, 1000);
+	}
+}
+
+function iniciar() 
+{
+	maximo=700;
+	
+	medio=document.getElementById('medio');
+	barra=document.getElementById('barra');
+	progreso=document.getElementById('progreso');
+	play=document.getElementById('play');
+	silence=document.getElementById('silenciar');
+	volumen=document.getElementById('volumen');
+	lightbox=document.getElementById("lightbox")
+    /* obtener los objetos del resto de elementos necesarios */
+	
+	play.addEventListener('click', accionPlay, false);
+	/* crear los manejadores de eventos para el resto de botones */
+
+	barra.addEventListener('click', desplazarMedio, false);
+}
+
+function reiniciar(){
+	medio.currentTime=0;
+    progreso.style.width='0px';
+    play.value='\u25BA';
+    window.clearInterval(bucle);
+    medio.pause();
+    medio.load();
+    redimensionaBarra();
+}
+
+function retroceder(){
+	var nuevoTiempo=medio.currentTime-5;
+    if(nuevoTiempo<0) nuevoTiempo=0;
+    medio.currentTime=nuevoTiempo;
+    progreso.style.width=nuevoTiempo*maximo / medio.duration+'px';
+}
+
+function avanzar(){
+	var nuevoTiempo=medio.currentTime+5;
+    if(nuevoTiempo<0) nuevoTiempo=0;
+    medio.currentTime=nuevoTiempo;
+    progreso.style.width=nuevoTiempo*maximo / medio.duration+'px';
+}
+
+function silenciar(){
+	if(silence.value == "silenciar"){
+	medio.volume = 0;
+	silence.value = "desilenciar"
+	} else {
+		medio.volume = 1;
+		silence.value = "silenciar";
+	}
+}
+
+function volumeUp(){
+	if(medio.volume < 1){
+    medio.volume += 0.1;
+    }
+}
+
+function volumeDown(){
+	if(medio.volume > 0){
+    medio.volume -= 0.1;
+    }
+}
+
+function ocultar(){
+	if (lightbox.style.display == "none"){
+		lightbox.style.display = "flex";
+	} else {
+		lightbox.style.display = "none";
+		reiniciar();
+	}
+	
+}
+
+window.addEventListener('load', iniciar, false);
